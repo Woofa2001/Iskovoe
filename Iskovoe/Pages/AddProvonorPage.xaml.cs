6a81a349c8be8834aa0efd04c668f432e4d987ac
name <mail>
2022-06-02 23:57:46 +0300

    Add document Word

--- Iskovoe/Pages/AddProvonorPage.xaml.cs
+++ Iskovoe/Pages/AddProvonorPage.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Iskovoe.Pages
 {
     /// <summary>
     /// Логика взаимодействия для AddProvonorPage.xaml
     /// </summary>
-    public partial class AddProvonorPage : Page
+    public partial class AddProvonorPage : System.Windows.Controls.Page
     {
         public AddProvonorPage()
         {
@@ -32,6 +33,124 @@ namespace Iskovoe.Pages
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void AddPravonorButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateDocument();
+        }
+
+        //Create document method  
+        private void CreateDocument()
+        {
+            try
+            {
+                //Create an instance for word app  
+                Word.Application winword = new Word.Application();
+
+                //Set animation status for word application  
+                winword.ShowAnimation = false;
+
+                //Set status for word application is to be visible or not.  
+                winword.Visible = false;
+
+                //Create a missing variable for missing value  
+                object missing = System.Reflection.Missing.Value;
+
+                //Create a new document  
+                Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                //Add header into the document  
+                foreach (Word.Section section in document.Sections)
+                {
+                    //Get the header range and add the header details.  
+                    Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    headerRange.Fields.Add(headerRange, Word.WdFieldType.wdFieldPage);
+                    headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    headerRange.Font.ColorIndex = Word.WdColorIndex.wdBlue;
+                    headerRange.Font.Size = 10;
+                    headerRange.Text = "Header text goes here";
+                }
+
+                //Add the footers into the document  
+                foreach (Word.Section wordSection in document.Sections)
+                {
+                    //Get the footer range and add the footer details.  
+                    Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    footerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkRed;
+                    footerRange.Font.Size = 10;
+                    footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    footerRange.Text = "Footer text goes here";
+                }
+
+                //adding text to document  
+                document.Content.SetRange(0, 0);
+                document.Content.Text = "This is test document " + Environment.NewLine;
+
+                //Add paragraph with Heading 1 style  
+                //Microsoft.Office.Interop.Word.Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
+                //object styleHeading1 = "Heading 1";
+                //para1.Range.set_Style(ref styleHeading1);
+                //para1.Range.Text = "Para 1 text";
+                //para1.Range.InsertParagraphAfter();
+
+                //Add paragraph with Heading 2 style  
+                //Microsoft.Office.Interop.Word.Paragraph para2 = document.Content.Paragraphs.Add(ref missing);
+                //object styleHeading2 = "Heading 2";
+                //para2.Range.set_Style(ref styleHeading2);
+                //para2.Range.Text = "Para 2 text";
+                //para2.Range.InsertParagraphAfter();
+
+                //Create a 5X5 table and insert some dummy record  
+                //Microsoft.Office.Interop.Word.Table firstTable = document.Tables.Add(para1.Range, 5, 5, ref missing, ref missing);
+
+                //firstTable.Borders.Enable = 1;
+                //foreach (Row row in firstTable.Rows)
+                //{
+                //    foreach (Cell cell in row.Cells)
+                //    {
+                //        //Header row  
+                //        if (cell.RowIndex == 1)
+                //        {
+                //            cell.Range.Text = "Column " + cell.ColumnIndex.ToString();
+                //            cell.Range.Font.Bold = 1;
+                //            //other format properties goes here  
+                //            cell.Range.Font.Name = "verdana";
+                //            cell.Range.Font.Size = 10;
+                //            //cell.Range.Font.ColorIndex = WdColorIndex.wdGray25;                              
+                //            cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;
+                //            //Center alignment for the Header cells  
+                //            cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                //            cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+
+                //        }
+                //        //Data row  
+                //        else
+                //        {
+                //            cell.Range.Text = (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
+                //        }
+                //    }
+                //}
+
+                //Save the document  
+                //object filename = AppDomain.CurrentDomain.BaseDirectory + @"Document\3.docx";
+                object filename =@"С:\3.docx";
+                document.SaveAs2(ref filename);
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                MessageBox.Show("Document created successfully !");
+            }   
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
