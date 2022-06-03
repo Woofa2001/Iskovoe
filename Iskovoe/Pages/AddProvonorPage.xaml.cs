3d1842e7cb25e669b2b6ae6352c482a5f8816a34
name <mail>
2022-06-03 22:37:51 +0300

    2

--- Iskovoe/Pages/AddProvonorPage.xaml.cs
+++ Iskovoe/Pages/AddProvonorPage.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Iskovoe.Pages
 {
     /// <summary>
     /// Логика взаимодействия для AddProvonorPage.xaml
     /// </summary>
-    public partial class AddProvonorPage : Page
+    public partial class AddProvonorPage : System.Windows.Controls.Page
     {
         public AddProvonorPage()
         {
@@ -32,6 +33,77 @@ namespace Iskovoe.Pages
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void AddPravonorButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateDocument();
+        }
+
+        //Create document method  
+        private void CreateDocument()
+        {
+            try
+            {
+                //Create an instance for word app  
+                Word.Application winword = new Word.Application();
+
+                //Set animation status for word application  
+                winword.ShowAnimation = false;
+
+                //Set status for word application is to be visible or not.  
+                winword.Visible = false;
+
+                //Create a missing variable for missing value  
+                object missing = System.Reflection.Missing.Value;
+
+                //Create a new document  
+                Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                //Add header into the document  
+                foreach (Word.Section section in document.Sections)
+                {
+                    //Get the header range and add the header details.  
+                    Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    headerRange.Fields.Add(headerRange, Word.WdFieldType.wdFieldPage);
+                    headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    headerRange.Font.ColorIndex = Word.WdColorIndex.wdBlue;
+                    headerRange.Font.Size = 10;
+                    headerRange.Text = "Header text goes here";
+                }
+
+                //Add the footers into the document  
+                foreach (Word.Section wordSection in document.Sections)
+                {
+                    //Get the footer range and add the footer details.  
+                    Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    footerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkRed;
+                    footerRange.Font.Size = 10;
+                    footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    footerRange.Text = "Footer text goes here";
+                }
+
+                //adding text to document  
+                document.Content.SetRange(0, 0);
+                document.Content.Text = "This is test document " + Environment.NewLine; 
+
+                //Save the document  
+                document.Save();
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                MessageBox.Show("Document created successfully !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
